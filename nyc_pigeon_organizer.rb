d02f40a96ca8e0733295292a8edb59e7343333fe
Austin Osorio <osorio_austin@yahoo.com>
2020-01-22 22:11:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,7 +18,7 @@ def nyc_pigeon_organizer(data)
     name.each do |name|
       pigeon_list.each do |bird, attributes|
         if name == bird
-            pigeon_list[bird][:color] << color.to_str
+            pigeon_list[bird][:color] << color.to_
         end
       end
     end
