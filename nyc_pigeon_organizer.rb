93c71e5d78dbfdae352db5b957c11b5b31404bcc
Austin Osorio <osorio_austin@yahoo.com>
2020-01-22 21:44:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,10 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
 
   data.each do |collection, attributes|
-    attributes.each
+    attributes.each do |attributes, type|
+      type.each do |type, name|
+
+      end
+    end
   end
 end
